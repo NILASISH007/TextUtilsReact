6d781013e3c1c51a88c12ab66e45cee39392c0ea
NILASISH BANERJEE <nilasishbanerjee1234@gmail.com>
2024-09-03 00:02:46 +0530

    route commit

--- src/App.js
+++ src/App.js
@@ -41,13 +41,13 @@ function App() {
     <>
      <Router>
     <Navbar tittle="TextUtils" aboutText ="About TextUtils" mode={mode} toggleMode={ToggleSwitch}/>
-    {/* <Alert alert={alert}/> */}
+    <Alert alert={alert}/>
      
         {/* <Navbar title="TextUtils" aboutText="About TextUtils" mode={mode} toggleMode={ToggleSwitch} /> */}
-        <Alert alert={alert} />
+        {/* <Alert alert={alert} /> */}
         <div className="container my-3">
           <Routes>
-            <Route exact path="/" element={<TextForm heading="Enter Your Text To Analyze" showAlert={showAlert} mode={mode} />} />
+          <Route exact path="/" element={<TextForm heading="Enter Your Text To Analyze" showAlert={showAlert} mode={mode}/>}/>
             <Route exact path="/about" element={<About />} />
           </Routes>
         </div>
